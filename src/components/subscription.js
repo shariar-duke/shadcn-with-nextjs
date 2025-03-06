0ab0cd48728d9b41b05f7b440505e5384a2c9316
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-06 09:39:31 +0600

    initial subscription form

--- src/components/subscription.js
+++ src/components/subscription.js
@@ -1,3 +1,55 @@
+"use client";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "./ui/button";
+const formSchema = z.object({
+  fullName: z
+    .string()
+    .min(3, { message: "Name must be at leasat 3 characters" }),
+  email: z.string().email({ message: "Invalid email address" }),
+});
 export default function SubscriptionForm() {
-  return <div>S</div>;
+  //defining the form
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: { fullName: "", email: "" },
+  });
+
+  // definign the submit handler
+
+  function onSubmit(values) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 }
