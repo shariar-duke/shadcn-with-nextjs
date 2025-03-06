5c6069c1aa6ab7e5ad6b87b77ac29d3287062509
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-06 10:44:58 +0600

    swithcer added in Navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,9 @@
+import { ModeToggle } from "./theme-switcher";
 export default function Navbar() {
   return (
     <div className="p-1 flex justify-between border-b">
       <h1 className="text-2xl">Subscribe to tapascript</h1>
+      <ModeToggle />
     </div>
   );
 }
