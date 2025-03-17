81fa78bb0d110f1d5306eeaf6b010afdb0601df8
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-18 01:41:46 +0600

    new user created as not found

--- src/app/actions/email.js
+++ src/app/actions/email.js
@@ -1,9 +1,24 @@
 "use server";
+import { Subscriber } from "../../../models/subs-model";
 export async function sendEmail(formData) {
   try {
     const email = formData["email"];
     const fullName = formData["fullName"];
-    console.log(email, formData);
+
+    if (!email) return null;
+
+    const foundSubscriber = await Subscriber.findOne({ email: email }).lean();
+
+    console.log(foundSubscriber);
+
+    if (!foundSubscriber) {
+      const subscribersPayload = {
+        name: fullName,
+        email,
+      };
+
+      await Subscriber.create(subscribersPayload);
+    }
   } catch (e) {
     throw new Error(e.message);
   }
