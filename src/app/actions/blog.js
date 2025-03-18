f84258b662205488bd88bec37262d8c2dab44b0f
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-18 12:06:20 +0600

    new server acton for button click

---
+++ src/app/actions/blog.js
@@ -0,0 +1,11 @@
+"use server";
+
+export async function addComment(comment, postId) {
+  console.log("Adding comment");
+  console.log(comment, postId);
+
+  // Make a DB call
+  // Store the comment
+
+  return true;
+}
