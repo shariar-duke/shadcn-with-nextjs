fecebe5bf5844935bb0db04fad55d9f00ce17bf0
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-18 14:24:43 +0600

    function bind

--- src/components/user-form.jsx
+++ src/components/user-form.jsx
@@ -1,9 +1,10 @@
 import { updateUser } from "@/app/actions/user";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
-export default function UserForm() {
+export default function UserForm({ userId }) {
+  const updatedUserWithId = updateUser.bind(null, userId);
   return (
-    <form action={updateUser} className="p-4 flex">
+    <form action={updatedUserWithId} className="p-4 flex">
       <Input className="w-1/2 mx-2" type="text" name="name" />
       <Button type="submit">Update User Name</Button>
     </form>
