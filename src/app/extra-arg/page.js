bd0164ef625c924eb6cb1833d1a20c6a3927355c
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-18 16:00:28 +0600

    id chagned

--- src/app/extra-arg/page.js
+++ src/app/extra-arg/page.js
@@ -2,7 +2,7 @@ import UserForm from "@/components/user-form";
 export default function ExtraArgsDemo() {
   return (
     <div>
-      <UserForm userId="1234" />
+      <UserForm userId="67d9198c8b0645efcd2edbd3" />
     </div>
   );
 }
