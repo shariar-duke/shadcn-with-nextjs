f4a7fce4ae2ba5f90747a89cbb69ec73570e24d2
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-18 15:35:06 +0600

    update logic in sever action

--- src/app/actions/user.js
+++ src/app/actions/user.js
@@ -1,7 +1,14 @@
 "use server";
+import { Subscriber } from "../../../models/subs-model";
 
 export async function updateUser(userId, formData) {
   const name = formData.get("name");
   console.log("User id is ", userId);
   console.log("The anme is ", name);
+
+  try {
+    await Subscriber.findByIdAndUpdate(userId, { name: name });
+  } catch (err) {
+    throw new Error(err.message);
+  }
 }
