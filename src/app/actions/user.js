d32892e2c9f754497952dd92b4dbb18f9275d310
Shariar Duke <shariarmahmud225@gmail.com>
2025-03-18 14:24:51 +0600

    fucntion bind

--- src/app/actions/user.js
+++ src/app/actions/user.js
@@ -1,7 +1,7 @@
 "use server";
 
-export async function updateUser(formData) {
+export async function updateUser(userId, formData) {
   const name = formData.get("name");
-
+  console.log("User id is ", userId);
   console.log("The anme is ", name);
 }
